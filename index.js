7dc9b0edfd7ea9fe71e6b88f7597d5cd2495212e
Malcom Wu <malcomwu@gmail.com>
2020-08-13 21:47:06 +0800

    Add test samples from musje.

---
+++ index.js
@@ -0,0 +1,6 @@
+
+// import appElement from './converterAppElement'
+// import appElement from './arrayElement'
+// import appElement from './todoAppElement'
+// import appElement from './attrTest'
+// import appElement from './inputTest'
